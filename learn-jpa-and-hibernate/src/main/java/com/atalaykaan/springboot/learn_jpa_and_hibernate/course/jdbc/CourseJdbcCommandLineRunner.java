5106bc4168cdffd07e09989317cd74c537f902d5
Kaan Efe Atalay <kaanefeatalay@gmail.com>
2025-09-19 01:14:48 +0300

    spring boot exercise - spring jdbc

--- learn-jpa-and-hibernate/src/main/java/com/atalaykaan/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/atalaykaan/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.atalaykaan.springboot.learn_jpa_and_hibernate.course.jdbc;
 
+import com.atalaykaan.springboot.learn_jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,15 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        courseJdbcRepository.insert();
+        courseJdbcRepository.insert(new Course(1, "Java ile Nesne Merkezli Programlama", "Akın"));
+        courseJdbcRepository.insert(new Course(2, "Spring", "in28minutes"));
+        courseJdbcRepository.insert(new Course(3, "Spring Boot", "Chad Darby"));
+
+        courseJdbcRepository.deleteById(1);
+
+        System.out.println(courseJdbcRepository.selectById(2));
+        System.out.println(courseJdbcRepository.selectById(3));;
+
+//        System.out.println(courseJdbcRepository.selectById(1));
     }
 }
--- learn-jpa-and-hibernate/src/main/java/com/atalaykaan/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/atalaykaan/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,6 +1,8 @@
 package com.atalaykaan.springboot.learn_jpa_and_hibernate.course.jdbc;
 
+import com.atalaykaan.springboot.learn_jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +14,19 @@ public class CourseJdbcRepository {
     private static String INSERT_QUERY =
             """
             insert into course(id, name, author)
-            values(1, 'spring boot course', 'in28minutes');
+            values(?, ?, ?);
+            """;
+
+    private static String DELETE_QUERY =
+            """
+            delete from course
+            where id=?
+            """;
+
+    private static String SELECT_QUERY =
+            """
+            select * from course
+            where id=?
             """;
 
     @Autowired
@@ -21,8 +35,17 @@ public class CourseJdbcRepository {
         this.courseJdbcTemplate = courseJdbcTemplate;
     }
 
-    public void insert() {
+    public void insert(Course course) {
 
-        courseJdbcTemplate.update(INSERT_QUERY);
+        courseJdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+    }
+
+    public void deleteById(long id) {
+        courseJdbcTemplate.update(DELETE_QUERY, id);
+    }
+
+    public Course selectById(long id) {
+
+        return courseJdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(Course.class), id);
     }
 }
