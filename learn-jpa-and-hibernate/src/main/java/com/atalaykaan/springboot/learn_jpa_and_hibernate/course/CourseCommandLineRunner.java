2f5eacdd1750aa7ab3f154cb201992d4e3bebc12
Kaan Efe Atalay <kaanefeatalay@gmail.com>
2025-09-19 02:55:13 +0300

    spring boot exercise - spring data jpa

--- learn-jpa-and-hibernate/src/main/java/com/atalaykaan/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/atalaykaan/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.atalaykaan.springboot.learn_jpa_and_hibernate.course;
 
 import com.atalaykaan.springboot.learn_jpa_and_hibernate.course.jdbc.CourseJdbcRepository;
 import com.atalaykaan.springboot.learn_jpa_and_hibernate.course.jpa.CourseJpaRepository;
+import com.atalaykaan.springboot.learn_jpa_and_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 
+//    ---------SPRING JDBC---------
+//
 //    private CourseJdbcRepository courseRepository;
 //
 //    @Autowired
@@ -16,11 +19,23 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //
 //        this.courseRepository = courseRepository;
 //    }
+//
+//    ---------SPRING JPA---------
+//
+//    private CourseJpaRepository courseRepository;
+//
+//    @Autowired
+//    public CourseCommandLineRunner(CourseJpaRepository courseRepository) {
+//
+//        this.courseRepository = courseRepository;
+//    }
+//
+//    ---------SPRING DATA JPA---------
 
-    private CourseJpaRepository courseRepository;
+    private CourseSpringDataJpaRepository courseRepository;
 
     @Autowired
-    public CourseCommandLineRunner(CourseJpaRepository courseRepository) {
+    public CourseCommandLineRunner(CourseSpringDataJpaRepository courseRepository) {
 
         this.courseRepository = courseRepository;
     }
@@ -28,15 +43,23 @@ public class CourseCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        courseRepository.insert(new Course(1, "Java ile Nesne Merkezli Programlama", "Akın"));
-        courseRepository.insert(new Course(2, "Spring", "in28minutes"));
-        courseRepository.insert(new Course(3, "Spring Boot", "Chad Darby"));
+        courseRepository.save(new Course(1, "Java ile Nesne Merkezli Programlama", "Akin"));
+        courseRepository.save(new Course(2, "Spring", "in28minutes"));
+        courseRepository.save(new Course(3, "Spring Boot", "Chad Darby"));
+        courseRepository.save(new Course(4, "Azure JPA", "in28minutes"));
 
-        courseRepository.deleteById(1);
+        courseRepository.deleteById(1L);
 
-        System.out.println(courseRepository.selectById(2));
-        System.out.println(courseRepository.selectById(3));;
+        System.out.println(courseRepository.findById(2L));
+        System.out.println(courseRepository.findById(3L));;
 
-//        System.out.println(courseRepository.selectById(1));
+//        System.out.println(courseRepository.findById(1L));
+
+        System.out.println(courseRepository.findByAuthor("Akin"));
+        System.out.println(courseRepository.findByAuthor("in28minutes"));
+        System.out.println(courseRepository.findByAuthor("Chad Darby"));
+
+        System.out.println(courseRepository.findByName("Spring"));
+
     }
 }
---
+++ learn-jpa-and-hibernate/src/main/java/com/atalaykaan/springboot/learn_jpa_and_hibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,14 @@
+package com.atalaykaan.springboot.learn_jpa_and_hibernate.course.springdatajpa;
+
+import com.atalaykaan.springboot.learn_jpa_and_hibernate.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByName(String name);
+
+    List<Course> findByAuthor(String author);
+
+}
