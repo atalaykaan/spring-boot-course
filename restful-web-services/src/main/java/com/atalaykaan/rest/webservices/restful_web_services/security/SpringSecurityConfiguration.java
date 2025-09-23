c50c500d682c7a0be325d1ee06089eb58cfe3bec
Kaan Efe Atalay <kaanefeatalay@gmail.com>
2025-09-23 14:25:57 +0300

    spring boot restful web services exercise

---
+++ restful-web-services/src/main/java/com/atalaykaan/rest/webservices/restful_web_services/security/SpringSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.atalaykaan.rest.webservices.restful_web_services.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                auth-> auth.anyRequest().authenticated()
+        );
+
+        http.httpBasic(withDefaults());
+
+        http.csrf(CsrfConfigurer::disable);
+
+        return http.build();
+    }
+}
